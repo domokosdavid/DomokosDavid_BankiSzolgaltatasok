7d9ee9d4acd55a2750f9a64dc5e8a2b9717e5c63
Domokos David <domokosdavid4@gmail.com>
2021-11-02 19:23:15 +0100

    Bank osztaly, szamlanyitas metodus letrehozasa, belefaradtam,nem tudtam befejezni

---
+++ src/com/company/Bank.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+
+    List<Szamla> szamlaLista = new ArrayList<>();
+
+    public Szamla szamlaNyitas(Tulajdonos tulajdonos, int hitelKeret){
+        Szamla szamla;
+
+            return szamla;
+        }
+
+    }
+}
