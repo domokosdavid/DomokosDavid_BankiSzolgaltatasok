f57488d8525d27498fa6897b8f38e5cb34235ac6
Domokos David <domokosdavid4@gmail.com>
2021-11-02 17:20:17 +0100

    getter,setter

--- src/com/company/MegtakaritasiSzamla.java
+++ src/com/company/MegtakaritasiSzamla.java
@@ -4,5 +4,17 @@ public class MegtakaritasiSzamla extends Szamla {
     private double kamat;
     private static double alapKamat = 1.05;
 
+    public MegtakaritasiSzamla(Tulajdonos tulajdonos) {
+        super(tulajdonos);
+        this.kamat = alapKamat;
+    }
+
+    public double getKamat() {
+        return kamat;
+    }
+
+    public void setKamat(double kamat) {
+        this.kamat = kamat;
+    }
 
 }
