7e5adb29d347efb896bef2fabaf3a307ef94c317
Domokos David <domokosdavid4@gmail.com>
2021-11-02 16:51:40 +0100

    Tulajdonos osztaly kivulrol irhato/olvashato

--- src/com/company/Tulajdonos.java
+++ src/com/company/Tulajdonos.java
@@ -1,5 +1,17 @@
 package com.company;
 
-public class Tulajdonos {
+public final class Tulajdonos {
+    private String nev;
 
+    public Tulajdonos(String nev) {
+        this.nev = nev;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
 }
